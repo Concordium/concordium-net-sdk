6d257a331024920edbdf0a4cb2462fc7bfe430c3
pavlop-takans <pavlo.paitak@tacans.com>
2022-04-25 20:40:12 +0300

    csdk-24 implement base projects skeleton with concordium grpc proto

---
+++ README.md
@@ -0,0 +1,5 @@
+# concordium-net-sdk
+
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.0-4baaaa.svg)](https://github.com/Concordium/.github/blob/main/.github/CODE_OF_CONDUCT.md)
+
+Represents an API wirten on C# for interacting with the [Concordium Node](https://github.com/Concordium/concordium-node).
---
+++ src/ConcordiumNetSdk.TestConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Concordium;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+var grpcChannel = GrpcChannel.ForAddress("http://localhost:10001");
+var client = new P2P.P2PClient(grpcChannel);
+var callOptions = new CallOptions(new Metadata {{ "authentication", "rpcadmin" }});
+
+var response = client.GetAccountInfo(
+    new GetAddressInfoRequest
+    {
+        Address = "3rAsvTuH2gQawenRgwJQzrk9t4Kd2Y1uZYinLqJRDAHZKJKEeH",
+        BlockHash = "44c52f0dc89c5244b494223c96f037b5e312572b4dc6658abe23832e3e5494af"
+    },
+    callOptions);
+
+Console.WriteLine(response.Value);
