692498b16d95b3ac23b878d5fc16637a99b7c7ed
pavlop-takans <pavlo.paitak@tacans.com>
2022-04-25 21:02:10 +0300

    csdk-24 implement concordium node client

--- src/ConcordiumNetSdk.TestConsoleApp/Program.cs
+++ src/ConcordiumNetSdk.TestConsoleApp/Program.cs
@@ -1,17 +1,8 @@
-﻿using Concordium;
-using Grpc.Core;
-using Grpc.Net.Client;
+﻿using ConcordiumNetSdk;
 
-var grpcChannel = GrpcChannel.ForAddress("http://localhost:10001");
-var client = new P2P.P2PClient(grpcChannel);
-var callOptions = new CallOptions(new Metadata {{ "authentication", "rpcadmin" }});
-
-var response = client.GetAccountInfo(
-    new GetAddressInfoRequest
-    {
-        Address = "3rAsvTuH2gQawenRgwJQzrk9t4Kd2Y1uZYinLqJRDAHZKJKEeH",
-        BlockHash = "44c52f0dc89c5244b494223c96f037b5e312572b4dc6658abe23832e3e5494af"
-    },
-    callOptions);
-
-Console.WriteLine(response.Value);
+var connection = new Connection
+{
+    Address = "http://localhost:10001",
+    AuthenticationToken = "rpcadmin"
+};
+var concordiumNodeClient = new ConcordiumNodeClient(connection);
---
+++ src/ConcordiumNetSdk/ConcordiumNodeClient.cs
@@ -0,0 +1,53 @@
+﻿using Concordium;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+namespace ConcordiumNetSdk;
+
+public class ConcordiumNodeClient : IDisposable
+{
+    private readonly P2P.P2PClient _client;
+    private readonly Metadata _metadata;
+    private readonly GrpcChannel _grpcChannel;
+
+    public ConcordiumNodeClient(Connection connection)
+    {
+        _metadata = new Metadata
+        {
+            {"authentication", connection.AuthenticationToken}
+        };
+
+        // todo: add all props from here to connection class
+        var options = new GrpcChannelOptions
+        {
+            Credentials = ChannelCredentials.Insecure
+        };
+
+        _grpcChannel = GrpcChannel.ForAddress(connection.Address, options);
+        _client = new P2P.P2PClient(_grpcChannel);
+    }
+
+    #region IDisposable Support
+
+    private bool _disposedValue;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                _grpcChannel.Dispose();
+            }
+
+            _disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+
+    #endregion
+}
