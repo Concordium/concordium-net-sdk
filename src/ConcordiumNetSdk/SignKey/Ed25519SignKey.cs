ef732e65bff32b821e8679ddb18b005c720d92a7
pavlop-takans <pavlo.paitak@tacans.com>
2022-05-09 00:38:11 +0300

    csdk-154 implement key signer

--- src/ConcordiumNetSdk/SignKey/Ed25519SignKey.cs
+++ src/ConcordiumNetSdk/SignKey/Ed25519SignKey.cs
@@ -1,9 +1,11 @@
+using NSec.Cryptography;
+
 namespace ConcordiumNetSdk.SignKey;
 
 /// <summary>
 /// Represents a hex encoded ed25519 sign key.
 /// </summary>
-public record Ed25519SignKey
+public record Ed25519SignKey : ISigner
 {
     private const int StringLength = 64;
     private const int BytesLength = 64;
@@ -12,20 +14,20 @@ public record Ed25519SignKey
     private readonly byte[] _value;
 
     /// <summary>
-    /// Creates an instance from a hex encoded string ed25519 sign key.
+    /// Creates an instance from a hex encoded string ed25519 representing sign key.
     /// </summary>
-    /// <param name="signKeyAsHexString">sign key as hex encoded string (64 characters).</param>
+    /// <param name="signKeyAsHexString">the sign key as hex encoded string (64 characters).</param>
     public Ed25519SignKey(string signKeyAsHexString)
     {
         if (signKeyAsHexString.Length != StringLength) throw new ArgumentException($"The sign key hex encoded string length must be {StringLength}.");
         _value = Convert.FromHexString(signKeyAsHexString);
         _formatted = signKeyAsHexString;
     }
-    
+
     /// <summary>
-    /// Creates an instance from a 64 byte ed25519 sign key.
+    /// Creates an instance from a 64 byte ed25519 representing sign key.
     /// </summary>
-    /// <param name="signKeyAsBytes">sign key as 64 bytes.</param>
+    /// <param name="signKeyAsBytes">the sign key as 64 bytes.</param>
     public Ed25519SignKey(byte[] signKeyAsBytes)
     {
         if (signKeyAsBytes.Length != BytesLength) throw new ArgumentException($"The sign key bytes length must be {BytesLength}.");
@@ -43,5 +45,13 @@ public record Ed25519SignKey
     /// </summary>
     public byte[] AsBytes => _value;
     
+    // todo: implement tests
+    public byte[] Sign(byte[] bytes)
+    {
+        Ed25519 algorithm = SignatureAlgorithm.Ed25519;
+        using Key key = Key.Import(algorithm, _value, KeyBlobFormat.RawPrivateKey);
+        return algorithm.Sign(key, bytes);
+    }
+
     public override string ToString() => AsString;
 }
