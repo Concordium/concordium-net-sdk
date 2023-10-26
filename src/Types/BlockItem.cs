b7d47a5c996a78eee732107af76ea1d635016c74
rasmus-kirk <mail@rasmuskirk.com>
2023-10-26 11:24:40 +0200

    Worked on adding credential deployment

--- src/Types/BlockItem.cs
+++ src/Types/BlockItem.cs
@@ -1,28 +1,41 @@
-//using Concordium.Sdk.Exceptions;
+using Concordium.Sdk.Exceptions;
 using Concordium.Sdk.Transactions;
-//using GrpcEffect = Concordium.Grpc.V2.PendingUpdate.EffectOneofCase;
+using GrpcPayload = Concordium.Grpc.V2.CredentialDeployment.PayloadOneofCase;
 
 namespace Concordium.Sdk.Types;
+
+public record CredentialDeployment(TransactionTime MessageExpiry, ICredentialPayload Payload) {
+    internal static CredentialDeployment From(Grpc.V2.CredentialDeployment cred) =>
+        new CredentialDeployment(
+            TransactionTime.From(cred.MessageExpiry),
+            cred.PayloadCase switch {
+                GrpcPayload.RawPayload => CredentialRawPayload.From(cred.RawPayload)
+                GrpcPayload.None => throw new NotImplementedException(),
+                _ => throw new MissingEnumException<CredentialPayload>(cred.PayloadCase),
+            }
+            
+        );
+}
+
+/// <summary> The payload of a Credential Deployment. </summary>
+public interface ICredentialPayload{};
+
 /// <summary>
-/// Minimum stake needed to become a baker. This only applies to protocol version 1-3.
+/// A raw payload, which is just the encoded payload.
+/// A typed variant might be added in the future.
 /// </summary>
-/// <param name="MinimumThresholdForBaking">Minimum threshold required for registering as a baker.</param>
-public record BakerStakeThreshold(CcdAmount MinimumThresholdForBaking)
-{
-    internal static BakerStakeThreshold From(Grpc.V2.BakerStakeThreshold bakerStakeThreshold) => new(CcdAmount.From(bakerStakeThreshold.BakerStakeThreshold_));
-};
+public sealed record CredentialRawPayload(byte[] RawPayload) : ICredentialPayload;
 
 /// <summary>
 /// A pending update.
 /// </summary>
 /// <param name="EffectiveTime">The effective time of the update.</param>
 /// <param name="Effect">The effect of the update.</param>
-//public sealed record BlockItem(TransactionHash TransactionHash, BlockItemType BlockItemType])
-//{
-//    internal static BlockItem From(Grpc.V2.BlockItem blockItem) => { 
-//        throw new NotImplementedException();
-//    }
-//}
+public sealed record BlockItem(TransactionHash TransactionHash, BlockItemType BlockItemType)
+{
+    internal static BlockItem From(Grpc.V2.BlockItem blockItem) =>
+        throw new NotImplementedException();
+}
 
 /// <summary>The effect of the update.</summary>
 public abstract record BlockItemType;
