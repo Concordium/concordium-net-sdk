23556951f13c91f2ef334e6f875fd36d59829da9
schwartz-concordium <132270889+schwartz-concordium@users.noreply.github.com>
2023-11-03 13:21:04 +0100

    Added code to parse modules

--- src/Types/VersionedModuleSource.cs
+++ src/Types/VersionedModuleSource.cs
@@ -1,11 +1,81 @@
 using Concordium.Sdk.Exceptions;
+using WebAssembly;
 
 namespace Concordium.Sdk.Types;
 
 /// <summary>
 /// Contains source code of a versioned module where inherited classes are concrete versions.
 /// </summary>
-public abstract record VersionedModuleSource(byte[] Source);
+public abstract record VersionedModuleSource
+{
+    /// <summary>
+    /// Module source code.
+    /// </summary>
+    public byte[] Source { get; }
+    private readonly Lazy<Module> _module;
+
+    /// <summary>
+    /// Base constructor
+    /// </summary>
+    /// <param name="source">Module source code.</param>
+    protected VersionedModuleSource(byte[] source)
+    {
+        this.Source = source;
+        this._module =  new Lazy<Module>(this.GetWasmModule);
+    }
+
+    /// <summary>
+    /// Get possible module schema embedded in module source code.
+    /// </summary>
+    /// <returns>Possible module schema and module schema version</returns>
+    public (string Schema, ModuleSchemaVersion SchemaVersion)? GetModuleSchema() =>
+        this.ExtractSchemaFromWebAssemblyModule(this._module.Value);
+
+    private Module GetWasmModule()
+    {
+        using var stream = new MemoryStream(this.Source);
+        var moduleWasm = Module.ReadFromBinary(stream);
+        return moduleWasm;
+    }
+
+    /// <summary>
+    /// The module can contain a schema in one of two different custom sections.
+    /// The supported sections depend on the module version.
+    /// The schema version can be either defined by the section name or embedded into the actual schema:
+    /// - Both v0 and v1 modules support the section 'concordium-schema' where the schema includes the version.
+    ///   - For v0 modules this is always a v0 schema.
+    ///   - For v1 modules this can be a v1, v2, or v3 schema.
+    ///- V0 modules additionally support section 'concordium-schema-v1' which always contain a v0 schema (not a typo).
+    /// - V1 modules additionally support section 'concordium-schema-v2' which always contain a v1 schema (not a typo).
+    /// The section 'concordium-schema' is the most common and is what the current tooling produces.
+    /// </summary>
+    private protected abstract (string Schema, ModuleSchemaVersion SchemaVersion)?
+        ExtractSchemaFromWebAssemblyModule(Module module);
+
+    /// <summary>
+    /// From custom sections in <see cref="module"/> get entry with name <see cref="entryKey"/>.
+    ///
+    /// Fails if multiple entries exist with the same name.
+    /// </summary>
+    /// <param name="module">Web assembly module</param>
+    /// <param name="entryKey">Name which is search for in custom sections.</param>
+    /// <param name="schema">Possible schema if exist in custom sections.</param>
+    /// <returns>True if schema was embedded in the custom section.</returns>
+    protected static bool GetSchemaFromWasmCustomSection(WebAssembly.Module module, string entryKey, out string? schema)
+    {
+        schema = null;
+        var customSection = module.CustomSections
+            .SingleOrDefault(section => section.Name.Equals(entryKey, StringComparison.InvariantCulture));
+
+        if (customSection == null)
+        {
+            return false;
+        }
+
+        schema = Convert.ToHexString(customSection.Content.ToArray());
+        return true;
+    }
+}
 
 internal static class VersionedModuleSourceFactory
 {
@@ -29,6 +99,19 @@ public sealed record ModuleV0(byte[] Source) : VersionedModuleSource(Source)
 {
     internal static ModuleV0 From(Grpc.V2.VersionedModuleSource.Types.ModuleSourceV0 moduleSourceV0) =>
         new(moduleSourceV0.Value.ToByteArray());
+
+    private protected override (string Schema, ModuleSchemaVersion SchemaVersion)? ExtractSchemaFromWebAssemblyModule(WebAssembly.Module module)
+    {
+        if (GetSchemaFromWasmCustomSection(module, "concordium-schema", out var moduleV0SchemaUndefined))
+        {
+            return (moduleV0SchemaUndefined!, ModuleSchemaVersion.Undefined); // always v0
+        }
+        if (GetSchemaFromWasmCustomSection(module, "concordium-schema-v1", out var moduleV0SchemaV0))
+        {
+            return (moduleV0SchemaV0!, ModuleSchemaVersion.V0); // v0 (not a typo)
+        }
+        return null;
+    }
 }
 
 /// <summary>
@@ -39,4 +122,17 @@ public sealed record ModuleV1(byte[] Source) : VersionedModuleSource(Source)
 {
     internal static ModuleV1 From(Grpc.V2.VersionedModuleSource.Types.ModuleSourceV1 moduleSourceV1) =>
         new(moduleSourceV1.Value.ToByteArray());
+
+    private protected override (string Schema, ModuleSchemaVersion SchemaVersion)? ExtractSchemaFromWebAssemblyModule(Module module)
+    {
+        if (GetSchemaFromWasmCustomSection(module, "concordium-schema", out var moduleV1SchemaUndefined))
+        {
+            return (moduleV1SchemaUndefined!, ModuleSchemaVersion.Undefined); // v1, v2, or v3
+        }
+        if (GetSchemaFromWasmCustomSection(module, "concordium-schema-v2", out var moduleV1SchemaV1))
+        {
+            return (moduleV1SchemaV1!, ModuleSchemaVersion.V1); // v1 (not a typo)
+        }
+        return null;
+    }
 }
---
+++ tests/UnitTests/Types/VersionedModuleSourceTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Concordium.Sdk.Types;
+using FluentAssertions;
+using Xunit;
+
+namespace Concordium.Sdk.Tests.UnitTests.Types;
+
+public sealed class VersionedModuleSourceTests
+{
+    [Theory]
+    [InlineData("cis1-wccd-embedded-schema-v0-unversioned.wasm", ModuleSchemaVersion.V0, false, 0)]
+    [InlineData("cis1-wccd-embedded-schema-v0-versioned.wasm.v0", ModuleSchemaVersion.Undefined, true, 0)]
+    [InlineData("cis2-wccd-embedded-schema-v1-unversioned.wasm.v1", ModuleSchemaVersion.V1, true, 1)]
+    [InlineData("cis2-wccd-embedded-schema-v1-versioned.wasm.v1", ModuleSchemaVersion.Undefined, true, 1)]
+    public async Task WhenCreateModuleSchema_ThenParseWithCorrectVersion(string fileName, ModuleSchemaVersion version, bool trim, int moduleVersion)
+    {
+        var bytes = (await File.ReadAllBytesAsync($"./Data/{fileName}"));
+        if (trim)
+        {
+            bytes = bytes[8..];
+        }
+        VersionedModuleSource module = moduleVersion == 0 ? new ModuleV0(bytes) : new ModuleV1(bytes);
+
+        // Act
+        var moduleSchema = module.GetModuleSchema();
+
+        // Assert
+        moduleSchema!.Value.SchemaVersion.Should().Be(version);
+    }
+
+
+    [Theory]
+    [InlineData("module.schema_embedded.wasm.hex", "FFFF03010000000C00000054657374436F6E7472616374000000000001150200000003000000466F6F020300000042617202")]
+    [InlineData("module.wasm.hex", null)]
+    public async Task WhenCreateModuleSchema_ThenSchemaPresent(string fileName, string? schema)
+    {
+        // Arrange
+        var load = (await File.ReadAllTextAsync($"./Data/{fileName}")).Trim();
+        var module = new ModuleV1(Convert.FromHexString(load));
+
+        // Act
+        var moduleSchema = module.GetModuleSchema();
+
+        // Assert
+        if (schema is not null)
+        {
+            moduleSchema.Should().NotBeNull();
+            moduleSchema!.Value.Schema.Should().Be(schema);
+        }
+        else
+        {
+            moduleSchema.Should().BeNull();
+        }
+    }
+}
