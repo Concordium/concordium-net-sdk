48aef7c7511224344b4a495de8423e4e167c7c56
Emil Holm Gjørup <eg@concordium.com>
2024-01-25 14:01:10 +0100

    Support update contract transactions and provide minimal example

--- src/Types/ContractAddress.cs
+++ src/Types/ContractAddress.cs
@@ -1,3 +1,6 @@
+using Concordium.Sdk.Helpers;
+using System.Buffers.Binary;
+
 namespace Concordium.Sdk.Types;
 
 /// <summary>
@@ -21,9 +24,45 @@ public sealed record ContractAddress(ulong Index, ulong SubIndex) : IAddress
     internal static ContractAddress From(Grpc.V2.ContractAddress contractAddress) => new(contractAddress.Index, contractAddress.Subindex);
 
     /// <summary>
+    /// Byte size of <see cref="ContractAddress"/>.
+    /// </summary>
+    public const uint BytesLength = 16;
+
+    /// <summary>
     /// Converts the contract address to its corresponding protocol buffer message instance.
     ///
     /// This can be used as the input for class methods of <see cref="Client.RawClient"/>.
     /// </summary>
     public Grpc.V2.ContractAddress ToProto() => new() { Index = this.Index, Subindex = this.SubIndex };
+
+    /// <summary>
+    /// Attempt to deserialize a contract address from a span of bytes.
+    /// </summary>
+    /// <param name="bytes">The span of bytes.</param>
+    /// <param name="output">Where to write the result of the operation.</param>
+    public static bool TryDeserial(ReadOnlySpan<byte> bytes, out (ContractAddress? Address, string? Error) output)
+    {
+        if (bytes.Length < BytesLength)
+        {
+            var msg = $"Invalid length of input in `ContractAddress.TryDeserial`. Expected at least {BytesLength} bytes, found {bytes.Length}";
+            output = (null, msg);
+            return false;
+        };
+
+        var index = BinaryPrimitives.ReadUInt64BigEndian(bytes);
+        var subindex = BinaryPrimitives.ReadUInt64BigEndian(bytes);
+
+        output = (new ContractAddress(index, subindex), null);
+        return true;
+    }
+
+    /// <summary>
+    /// Serialize the ContractAddress in big-endian format.
+    /// </summary>
+    public byte[] ToBytes() {
+        using var memoryStream = new MemoryStream((int) BytesLength); // Safe to cast since a payload will never be
+        memoryStream.Write(Serialization.ToBytes(this.Index));
+        memoryStream.Write(Serialization.ToBytes(this.SubIndex));
+        return memoryStream.ToArray();
+    }
 }
--- src/Types/Parameter.cs
+++ src/Types/Parameter.cs
@@ -1,3 +1,6 @@
+using System.Buffers.Binary;
+using Concordium.Sdk.Helpers;
+
 namespace Concordium.Sdk.Types;
 
 /// <summary>
@@ -5,6 +8,67 @@ namespace Concordium.Sdk.Types;
 /// </summary>
 public sealed record Parameter(byte[] Param)
 {
+    /// <summary>
+    /// Construct an empty smart contract parameter.
+    /// </summary>
+    public static Parameter Empty() {
+        return new Parameter(new byte[0]);
+    }
+
+    private const uint MaxByteLength = 65535;
+    /// <summary>
+    /// Gets the serialized length (number of bytes) of the parameter.
+    /// </summary>
+    internal uint SerializedLength() => sizeof(ushort) + (uint)this.Param.Length;
+
+    /// <summary>
+    /// Gets the minimum serialized length (number of bytes) of the parameter.
+    /// </summary>
+    internal const uint MinSerializedLength = sizeof(ushort);
+
+    /// <summary>
+    /// Copies the parameters to a byte array which has the length preprended.
+    /// </summary>
+	public byte[] ToBytes()
+    {
+        using var memoryStream = new MemoryStream((int)this.SerializedLength());
+        memoryStream.Write(Serialization.ToBytes((ushort)this.Param.Length));
+        memoryStream.Write(this.Param);
+        return memoryStream.ToArray();
+    }
+
+    /// <summary>
+    /// Create a parameter from a byte array.
+    /// </summary>
+    /// <param name="bytes">The serialized parameters.</param>
+    /// <param name="output">Where to write the result of the operation.</param>
+    public static bool TryDeserial(ReadOnlySpan<byte> bytes, out (Parameter? Parameter, string? Error) output)
+    {
+        if (bytes.Length < MinSerializedLength)
+        {
+            var msg = $"Invalid length of input in `Parameter.TryDeserial`. Expected at least {MinSerializedLength}, found {bytes.Length}";
+            output = (null, msg);
+            return false;
+        };
+
+        var sizeRead = BinaryPrimitives.ReadUInt16BigEndian(bytes);
+        if (sizeRead > MaxByteLength) {
+            var msg = $"Invalid length of input in `Parameter.TryDeserial`. The parameter size can be at most {MaxByteLength} bytes, found {bytes.Length}";
+            output = (null, msg);
+            return false;
+        }
+
+        var size = sizeRead + MinSerializedLength;
+        if (size > bytes.Length)
+        {
+            var msg = $"Invalid length of input in `Parameter.TryDeserial`. Expected array of size at least {size}, found {bytes.Length}";
+            output = (null, msg);
+            return false;
+        };
+        output = (new Parameter(bytes.Slice(sizeof(ushort), sizeRead).ToArray()), null);
+        return true;
+    }
+
     /// <summary>
     /// Convert parameters to hex string.
     /// </summary>
